7b496fed292c21ef0937ee721fc32c43227f6197
Thomás Alonso <96887716+HyperSpaceGZ@users.noreply.github.com>
2023-06-11 17:05:56 -0300

    ai

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] private Transform Player;
+    [SerializeField] private NavMeshAgent EnemyNavMesh;
+    [SerializeField] private bool hastriggered;
+
+    [SerializeField] private int health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnemyNavMesh = GetComponent<NavMeshAgent>();
+        EnemyNavMesh.updateRotation = false;
+        EnemyNavMesh.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hastriggered == false && collision.gameObject.tag == "Player")
+        {
+            hastriggered = true;
+            InvokeRepeating("EnemyFollowerMovement", 0f, 0.02f);
+        }
+    }
+
+    private void EnemyFollowerMovement()
+    {
+        //Rotacion del sprite enemigo
+        Vector2 direction = Player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        //Movimiento
+        EnemyNavMesh.SetDestination(Player.position);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+    private void EnemyHit()
+    {
+
+    }
+
 }
