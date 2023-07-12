c41a8eed114a2c5b4a3c93e0e58bdb7047c981ec
Thomás Alonso <96887716+HyperSpaceGZ@users.noreply.github.com>
2023-07-11 23:08:09 -0300

    Menu 2

--- Assets/Scenes/MenuScript.cs
+++ Assets/Scenes/MenuScript.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image LoadingScreen;
+
+    private void Awake()
+    {
+        LoadingScreen.enabled = false;
+    }
+
     public void Play()
     {
+        LoadingScreen.enabled = true;
         SceneManager.LoadScene("Game");
     }
 
@@ -23,14 +31,24 @@ public class MenuScript : MonoBehaviour
 
     public void Level1()
     {
+        LoadingScreen.enabled = true;
         SceneManager.LoadScene("Game");
     }
     public void Level2()
     {
+        LoadingScreen.enabled = true;
         SceneManager.LoadScene("Game2");
     }
     public void Level3()
     {
+        LoadingScreen.enabled = true;
         SceneManager.LoadScene("Game3");
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
 }
