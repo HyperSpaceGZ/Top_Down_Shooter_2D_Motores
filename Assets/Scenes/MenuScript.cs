389c2f3348f4234efedd7f322474d0d2fc566a78
Thomás Alonso <96887716+HyperSpaceGZ@users.noreply.github.com>
2023-07-11 22:38:40 -0300

    menu 1

--- Assets/Scenes/MenuScript.cs
+++ Assets/Scenes/MenuScript.cs
@@ -15,4 +15,22 @@ public class MenuScript : MonoBehaviour
     {
         SceneManager.LoadScene("Menu");
     }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Game3");
+    }
 }
